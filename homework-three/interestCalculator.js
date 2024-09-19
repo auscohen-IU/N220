a99a43202d4898b872a8508dfd28554c33818f39
auscohen-IU <auscohen@iu.edu>
2024-09-18 22:43:02 -0400

    Updated .js and html files.

--- homework-three/interestCalculator.js
+++ homework-three/interestCalculator.js
@@ -1,23 +1,17 @@
 // Interest formula: Total = Principle(1+Rate*Time)
 
-const principalAmmount = document.getElementById("input1").value;
-const rateAmmount = document.getElementById("input2").value;
-const timeAmmount = document.getElementById("input3").value;
+// Set Principal, Rate, and Time to the numbers in inputs.
+const principalAmmount = parseFloat(document.getElementById("pInput").value);
+const growthRate = parseFloat(document.getElementById("rInput").value);
+const timeAmmount = parseFloat(document.getElementById("tInput").value);
 
-const totalInterest = 1 + rateAmmount + timeAmmount;
-const totalAmmount = principalAmmount * totalInterest;
 
-function interestTotal(){
-    document.getElementById("giveResult").innerHTML += 
-        "With a beginning principal of $" + 
-        principalAmmount + 
-        "and with a growth rate of " + 
-        rateAmmount + 
-        "% for" + 
-        timeAmmount + 
-        " years, your total interest will be $" + 
-        totalInterest + 
-        "with a grand total of $" + 
-        totalAmmount;
+// Interest rate is the formula below. grandTotal is the final amount of money someone will have.
+const totalInterest = (1 + (growthRate * timeAmmount));
 
+const grandTotal = (totalInterest * principalAmmount);
+
+// finalResults() should result in the text being displayed at the bottom of the page along with the calculations. 
+function finalResults(){
+    document.querySelector("#giveResult").innerHTML += "With a beginning principal of $" + principalAmmount + " and with a growth rate of " + growthRate + "% for " + timeAmmount + " years, your total interest will be $" + totalInterest + " with a grand total of $" + grandTotal + ". <br/><br/>";
 }
