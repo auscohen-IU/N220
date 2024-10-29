45792afd5b929ee8b5d9ee328a8f113ae2f02633
auscohen-IU <auscohen@iu.edu>
2024-10-29 17:07:23 -0400

    Added info into the MD, also added functions for the JS file that are supposed to display a square of a color. Having issues getting the square to display.

--- week-10/colorChanger.js
+++ week-10/colorChanger.js
@@ -1,5 +1,5 @@
 /**
- * @type { { currentColor: string, lastColor: string } [] }
+ * @type { { currentColor: string, lastColors: string } [] }
  * @description This is a list of colors that the user added.
  */
 
@@ -23,14 +23,14 @@ function addNewColorItem() {
     const newColorValue = newColorRef.value;
     console.log(newColorItem.value);
     
-    userColors.push({ currentColor: newColorValue });
+    userColors.push({ currentColor: newColorValue, });
     newColorRef.value = "#000000";
 
     showUserColors();
 }
 
 /**
- * @description Loops through userColors list and populates them on th page.
+ * @description Loops through userColors list and populates them on the page.
  */
 function showUserColors(){
     const colorSquares = document.getElementById("color-squares");
@@ -39,25 +39,40 @@ function showUserColors(){
     for (let i = 0; i < userColors.length; i++) {
         const userColor = userColors[i];
         colorSquares.innerHTML += 
-        `
-        <div style="height: 50px; width: 50px; background-color: ${userColor.currentColor}" onclick="openColorChanger(${i}")>
-            <input 
-                type="color" 
-                id="user-color-${i}" 
-                name="user-color-${i}" 
-                hidden 
-                value="${userColor.currentColor}"
-                onchange="changeUserColor(${i})"
-            />
-        </div>
-        `;
-    }
+        `<div>
+            <div style="height: 100px; width: 100px; padding: 50px: background-color: ${userColor.currentColor}" onclick="openColorChanger(${i})">
+                <input 
+                    type="color" 
+                    id="user-color-${i}" 
+                    name="user-color-${i}" 
+                    hidden 
+                    value="${userColor.currentColor}"
+                <div>
+                    <button onclick=changeUserColor(${i}")>Update Color</button>
+                    <button onclick=revertUserColor(${i}")>Revert Color</button>
+
+                </div>
+            </div>
+        </div>`;
+
+        showUserColors();
+    }; 
 }
 
 function changeUserColor(userColorIndex){
     const userColorRef = document.getElementById(`user-color-${userColorIndex}`);
-    console.log(userColorRef.value);
+
+    const lastColor = userColors[userColorIndex].currentColor;
+    const newUserColor = {
+        currentColor: userColorRef.value,
+        lastColor: lastColor, 
+    };
+    userColors.splice(userColorIndex, 1, newUserColor);
+
+    showUserColors();
 }
+
+function revertUserColor(){}
 
 function openColorChanger(userColorIndex){
     console.log(userColorIndex);
--- week-10/readme.md
+++ week-10/readme.md
@@ -11,3 +11,6 @@ Description:
         - Prof just casually opened this website and started a live preview of HTML, CSS, and JS to showcase putting an image in an HTML file. 
     - If someone uses a dark mode enabler, is it just a JS file that is placed after every other JS file that changes the background color to a dark theme?
     - I feel like we're going hard in the paint rn.
+    - Using FireFox has led to some problems about how the code is read on the browser. 
+    - Got *lost* in the sauce when we started going over the revertLastColor section of the JS file.
+    - Review **Loops!** The next few weeks are going to be how to implement API's and how to use in-browser debug things. 
