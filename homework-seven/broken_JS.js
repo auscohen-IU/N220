16d338b9d18236b126b8d740772f6ebba783b176
auscohen-IU <auscohen@iu.edu>
2024-11-04 17:55:33 -0500

    I think I've gone through and fixed everything. I added notes to make thinks more clear in the JS file.

--- homework-seven/broken_JS.js
+++ homework-seven/broken_JS.js
@@ -1,41 +1,50 @@
-let PlayerChoie = document.getElementsByClassName("choice");
-      let CompArray = (Rock, Paper, Scissors);
-      let innerscore = 0;
+let PlayerChoice = document.getElementsByClassName("choice");
+let CompArray = ["Rock", "Paper", "Scissors"];
+let innerscore = 0;
+console.log("0 = Loss, 1 = Win, 0.5 = Tie");
 
-      function RPS(numb) {
-        let CompDecision = CompArray[Math.floor(Math.random() * CompArray.length);
+function RPS(numb) {
+  let CompDecision = CompArray[Math.floor(Math.random() * CompArray.length)];
      
-        document.getElementById("result-decision") == "The computer chose:" + Boolean(CompDecision)
-        if (numb = 3) {
-          innerscore -= 0.5;
-        }
+  document.getElementById("resultDecision").innerHTML = "The computer chose: " + Boolean(CompDecision)
+    // I don't think this bit is necessary because we don't have four outcomes Only [0, 1, 2]. 
+      // if (numb === 3) {
+      //   innerscore = 0.5;
+      // }
 
-        if (num = 1) {
-          if (CompDecision == "Paper") {
-            innerscore - 1;
-          }
-          if (CompDecision == "Scissors") {
-            innerscore + 1;
-          }
+      // Player chooses Rock
+      if (numb === 0) {
+        if (CompDecision == "Paper") {
+        innerscore = 0; //Lose
         }
-
-        if (numb = "1") {
-          if (CompDecision == "Rock") {
-            innerscore += 1;
-          }
-           (CompDecision == "Scissors") {
-            innerscore - 1;
-          }
+        else if (CompDecision == "Scissors") {
+        innerscore = 1; //Win
         }
-
-        if (numb = 2) {
-          if (CompDecision == "Rock") {
-            innerscore -= 1;
-          }
-          else (CompDecision == "Paper") {
-            innerscore + 1;
-          
-        }
-
-        document.getElementsByClassName("Score").innerHTML = innrscore;
+        else {innerscore = 0.5} //Tie
       }
+
+      // Player chooses Paper
+      if (numb === 1) {
+        if (CompDecision == "Rock") {
+        innerscore = 1; //Win
+        }
+        else if (CompDecision == "Scissors") {
+        innerscore = 0; //Lose
+        }
+        else {innerscore = 0.5} //Tie
+      }
+
+      // Player chooses Scissors
+      if (numb === 2) {
+        if (CompDecision == "Rock") {
+        innerscore = 0; //Lose
+        }
+        else if (CompDecision == "Paper") {
+        innerscore = 1; //Win
+        }
+        else {innerscore = 0.5} //Tie
+      }
+
+      // Display score
+      document.getElementById("Score").innerHTML = innerscore;
+    }
--- homework-seven/readme.md
+++ homework-seven/readme.md
@@ -1,11 +1,12 @@
 Name:
-- 
+- Austin Cohen
 
 Date:
-- 
+- November 4, 2024
 
 URL:
-- 
+- http://127.0.0.1:5500/N220/homework-seven/RPSG.html 
 
 Description:
+- Debug the assignment until you get it to work. You **CANNOT** rewrite entire lines/blocks of code. You will make simple changes to make this work. 
 - 
