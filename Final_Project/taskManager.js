d128b7dd04780f2e6adda61c3e267ff4795da9db
auscohen-IU <auscohen@iu.edu>
2024-12-03 18:30:45 -0500

    More revision

--- Final_Project/taskManager.js
+++ Final_Project/taskManager.js
@@ -1,3 +1,18 @@
 // .js file connected?
-console.log("JavaScript file connected!");
+console.log("taskManager.js file connected!");
 
+// Username and Password values
+let username = document.getElementById("usernameInput").value;
+let password = document.getElementById("passwordInput").value;
+
+// Known Users
+const knownUsers = [
+    {username = "admin", password = "admin"}
+]
+
+const user = knownUsers.find(user => user.username === username)
+
+// Login button runs the check
+function loginCheck {
+    
+}
