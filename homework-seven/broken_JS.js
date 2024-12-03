420e62148a473612ef650c904101d494bdada07a
auscohen-IU <auscohen@iu.edu>
2024-12-03 18:57:02 -0500

    Corrected the .js file so that the return is no longer a boolean. Added context in the readme.md

--- homework-seven/broken_JS.js
+++ homework-seven/broken_JS.js
@@ -1,50 +1,46 @@
 let PlayerChoice = document.getElementsByClassName("choice");
 let CompArray = ["Rock", "Paper", "Scissors"];
-let innerscore = 0;
+let innerscore = 0;  // Initial score
 console.log("0 = Loss, 1 = Win, 0.5 = Tie");
 
 function RPS(numb) {
-  let CompDecision = CompArray[Math.floor(Math.random() * CompArray.length)];
+    let CompDecision = CompArray[Math.floor(Math.random() * CompArray.length)];
      
-  document.getElementById("resultDecision").innerHTML = "The computer chose: " + Boolean(CompDecision)
-    // I don't think this bit is necessary because we don't have four outcomes Only [0, 1, 2]. 
-      // if (numb === 3) {
-      //   innerscore = 0.5;
-      // }
+    document.getElementById("resultDecision").innerHTML = "The computer chose: " + CompDecision;
 
-      // Player chooses Rock
-      if (numb === 0) {
+    // Player chooses Rock
+    if (numb === 0) {
         if (CompDecision == "Paper") {
-        innerscore = 0; //Lose
+            innerscore = 0; // Lose
+        } else if (CompDecision == "Scissors") {
+            innerscore = 1; // Win
+        } else {
+            innerscore = 0.5; // Tie
         }
-        else if (CompDecision == "Scissors") {
-        innerscore = 1; //Win
-        }
-        else {innerscore = 0.5} //Tie
-      }
-
-      // Player chooses Paper
-      if (numb === 1) {
-        if (CompDecision == "Rock") {
-        innerscore = 1; //Win
-        }
-        else if (CompDecision == "Scissors") {
-        innerscore = 0; //Lose
-        }
-        else {innerscore = 0.5} //Tie
-      }
-
-      // Player chooses Scissors
-      if (numb === 2) {
-        if (CompDecision == "Rock") {
-        innerscore = 0; //Lose
-        }
-        else if (CompDecision == "Paper") {
-        innerscore = 1; //Win
-        }
-        else {innerscore = 0.5} //Tie
-      }
-
-      // Display score
-      document.getElementById("Score").innerHTML = innerscore;
     }
+
+    // Player chooses Paper
+    if (numb === 1) {
+        if (CompDecision == "Rock") {
+            innerscore = 1; // Win
+        } else if (CompDecision == "Scissors") {
+            innerscore = 0; // Lose
+        } else {
+            innerscore = 0.5; // Tie
+        }
+    }
+
+    // Player chooses Scissors
+    if (numb === 2) {
+        if (CompDecision == "Rock") {
+            innerscore = 0; // Lose
+        } else if (CompDecision == "Paper") {
+            innerscore = 1; // Win
+        } else {
+            innerscore = 0.5; // Tie
+        }
+    }
+
+    // Display the score
+    document.getElementById("score").innerHTML = "Score: " + innerscore;
+}
--- homework-seven/readme.md
+++ homework-seven/readme.md
@@ -3,10 +3,15 @@ Name:
 
 Date:
 - November 4, 2024
+- December 3, 2024
 
 URL:
-- http://127.0.0.1:5500/N220/homework-seven/RPSG.html 
+- http://127.0.0.1:5500/N220/homework-seven/homework-seven.html
 
 Description:
 - Debug the assignment until you get it to work. You **CANNOT** rewrite entire lines/blocks of code. You will make simple changes to make this work. 
-- 
+
+- Corrected the assignment so that the return is no longer a true or false boolean. 
+- I don't know what happened to my original copy of the HTML code, but it wasn't in my github repository, so I redid it for the assignment. 
+    - I know that it said not to change large chunks in the assignment description, but I changed the way to make user choice by making buttons.
+    - This translates better.
