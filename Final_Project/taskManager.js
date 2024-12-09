78165d07a3cb10ab928a28c9d192096b63e6da42
auscohen-IU <auscohen@iu.edu>
2024-12-09 18:20:24 -0500

    Hated the code and started over.

--- Final_Project/taskManager.js
+++ Final_Project/taskManager.js
@@ -1,19 +1,3 @@
 // .js file connected?
 console.log("taskManager.js file connected!");
 
-// Username and Password variables
-let username = document.getElementById("usernameInput").value;
-let password = document.getElementById("passwordInput").value;
-
-// Admin Login & Known Users
-const adminLogin = {
-    username:"admin",
-    password:"admin"
-};
-
- const knownUsers = [adminLogin];
-
-//  Login function
-function goodLogin(){
-    document.getElementById("dashboard").innerHTML += dashboard
-}
